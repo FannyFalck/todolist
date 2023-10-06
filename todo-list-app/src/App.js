cf102bb7e5feb1f2c1f3cbd9d527d45d48963579
FannyFalck <falckfanny@gmail.com>
2023-10-06 14:51:10 +0200

    front-end and fonts
    Added some awesome fonts, fixed the front-end.

--- todo-list-app/src/App.js
+++ todo-list-app/src/App.js
@@ -1,12 +1,25 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPenToSquare } from '@fortawesome/free-regular-svg-icons'; // För en vanlig ikon
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+
+
 import './Style.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Fannys, To do List</h1>
+        <h1> <u>Fannys, To do List </u></h1>
       </header>
+      <form className="ToDoform">
+        <input type="text"  className="todo-input" placeholder="Vad vill du lägga till idag?"/>
+        <button type="submit" className="SubmitButton">Lägg till </button>
+      </form>
+
+      <FontAwesomeIcon icon={faPenToSquare} style={{ color: "#000000" }} />
+      <FontAwesomeIcon icon={faTrash} style={{ color: "#000000" }} />
       
+
     </div>
 
     
